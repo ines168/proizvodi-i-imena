7509ccb30ebcde4b177ba76abb4614eb8dce9923
Ines <ines.cvek16@gmail.com>
2025-02-13 21:51:42 +0100

    handling empty array

--- src/Proizvodi/ProizvodMain.java
+++ src/Proizvodi/ProizvodMain.java
@@ -9,13 +9,16 @@ public class ProizvodMain {
         List<Proizvod> proizvodi = new ArrayList<Proizvod>();
 
         Scanner sc = new Scanner(System.in);
+        System.out.println("Proizvod ima naziv, cijenu i količinu. Unesi proizvode. ");
+        System.out.println("Završi unošenje sa 'stop'. \n");
 
         do {
             try {
                 System.out.println("Unesi naziv proizvoda: ");
                 String naziv = sc.nextLine();
 
-                if(naziv.equals("stop")) {
+                //stop prekida program
+                if(naziv.toLowerCase().equals("stop")) {
                     break;
                 }
 
@@ -24,19 +27,36 @@ public class ProizvodMain {
 
                 System.out.println("Unesite količinu proizvoda: ");
                 int kolicina = sc.nextInt();
+                //nextInt zajebava s enterom
                 sc.nextLine();
 
+                //kreira objekt i doda u listu proizvodi
                 proizvodi.add(new Proizvod(naziv, cijena, kolicina));
 
             } catch (Exception e) {
+                //Greške u tipu podatka ne prekida program
                 System.out.println("Krivi tip podatka, pokušaj ponovno ili izađi sa 'stop'.");
                 sc.nextLine();
             }
 
         } while(true);
 
-        for (Proizvod proizvod : proizvodi) {
-            System.out.println(proizvod);
+        if(proizvodi.size()==0) {
+            System.out.println("Niste unijeli niti jedan proizvod.");
+        } else {
+            //Izlistaj unešene proizvode toString metodom
+            System.out.println("toString:");
+            for (Proizvod proizvod : proizvodi) {
+                System.out.println(proizvod);
+            }
+
+            //Izlistaj unešene proizvode dohvati metodom
+            System.out.println("\n.dohvatiPodatke:");
+            for (Proizvod proizvod : proizvodi) {
+                System.out.println(proizvod.dohvatiPodatke());
+            }
         }
+
+
     }
 }
